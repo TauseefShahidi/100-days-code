13e28f19581d1cd7c7036610d5b263c5a87201e1
Tauseef shahidi <tauseefshahidi0786@gmail.com>
2026-07-13 21:14:01 +0530

    17th commit

---
+++ Day17.cpp
@@ -0,0 +1,47 @@
+Problem: Write a program to find the maximum and minimum values present in a given array of integers.
+
+Input:
+- First line: integer n
+- Second line: n integers
+
+Output:
+- Print the maximum and minimum elements
+
+Example:
+Input:
+6
+3 5 1 9 2 8
+
+Output:
+Max: 9
+Min: 1
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int arr[100];
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int max = arr[0];
+    int min = arr[0];
+
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > max)
+            max = arr[i];
+
+        if (arr[i] < min)
+            min = arr[i];
+    }
+
+    cout << "Max: " << max << endl;
+    cout << "Min: " << min << endl;
+
+    return 0;
+}
